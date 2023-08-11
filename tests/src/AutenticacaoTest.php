7bf4b208bd06cf9593701c5d422b4e0e74e5aec7
zlChurrOsCH <40037040+zlChurrOsCH@users.noreply.github.com>
2023-08-11 01:45:27 -0300

    Update AutenticacaoTest.php

--- tests/src/AutenticacaoTest.php
+++ tests/src/AutenticacaoTest.php
@@ -6,20 +6,36 @@ use Projeto\Twd\Autenticacao;
 class AutenticacaoTest extends TestCase {
     private $autenticacao;
 
+    // Configuração inicial antes de cada teste
     protected function setUp(): void {
         $this->autenticacao = new Autenticacao();
     }
 
     /**
+     * Testa diferentes cenários de autenticação.
+     *
      * @dataProvider dadosCenariosDeAutenticacao
+     *
+     * @param string $username Usuário para autenticação.
+     * @param string $password Senha para autenticação.
+     * @param string $esperado Mensagem de autenticação esperada.
      */
     public function testeCenariosDeAutenticacao($username, $password, $esperado) {
+        //echo "Usuário verificado!\n";
         $mensagem = $this->autenticacao->gerarMensagem($username, $password);
+        
+        // Verifica se a mensagem é igual ao esperado
         $this->assertEquals($esperado, $mensagem);
     }
 
-    public function dadosCenariosDeAutenticacao() {
+    /**
+     * Fornece dados de teste para diferentes cenários de autenticação.
+     *
+     * @return array Conjunto de dados de teste.
+     */
+    public static function dadosCenariosDeAutenticacao() {
         return [
+            // [Usuário, Senha, Mensagem Esperada]
             ['admin', 'senhaAdmin', 'Usuário autenticado como admin'],
             ['getuser', 'senhaGet', 'Usuário autenticado como getuser'],
             ['deleteuser', 'senhaDelete', 'Usuário autenticado como deleteuser'],
